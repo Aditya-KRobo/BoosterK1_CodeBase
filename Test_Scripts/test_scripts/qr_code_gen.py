413a814307f4f4cb253bcc3c965aa68029aa717e
Booster <booster@boosterobotics.com>
2026-05-04 11:35:31 +0800

    new qr codes generated

--- Test_Scripts/test_scripts/qr_code_gen.py
+++ Test_Scripts/test_scripts/qr_code_gen.py
@@ -1,5 +1,5 @@
 import qrcode
 
-data = "Crocodile"
+data = "Kangaroo"
 img = qrcode.make(data)
-img.save("/home/booster/Workspace/BoosterK1_CodeBase/Test_Scripts/res/qrcodes/Crocodile.png")
+img.save("/home/booster/Workspace/BoosterK1_CodeBase/Test_Scripts/res/qrcodes/Kangaroo.png")
